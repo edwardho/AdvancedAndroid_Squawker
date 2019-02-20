1040ba14214e90ee8eb8f3e3cccd0689394553b7
edwardho <ekinbenho@gmail.com>
2019-02-20 15:36:13 -0500

    Completed TFCM.03-Exercise

---
+++ app/src/main/java/android/example/com/squawker/fcm/SquawkFirebaseInstanceIdService.java
@@ -0,0 +1,24 @@
+package android.example.com.squawker.fcm;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class SquawkFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+    private  static String LOG_TAG = SquawkFirebaseInstanceIdService.class.getSimpleName();
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated Instane ID token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(LOG_TAG, "Refreshed token: " + refreshedToken);
+
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+
+    }
+}
